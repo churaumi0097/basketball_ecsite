d807ebc4ba6254bff1e73e8bf2c807dc784b15a7
churaumi0097 <111668959+churaumi0097@users.noreply.github.com>
2024-11-12 23:15:04 +0900

    Update settings.py

--- apl/settings.py
+++ apl/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y@)vhxv)x=q-5si86sd$*0s3$jfyjpl$6oa+*m9_z$el*)+joc"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
